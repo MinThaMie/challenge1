aed717ee60fe87db090564d57fdeb74b53a0daf4
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc3137.nedap.local>
2017-02-09 16:40:07 +0100

    jQuery works + added remove function (STUB)

--- pewe/options.js
+++ pewe/options.js
@@ -1,10 +1,12 @@
 function init() {
   var blacklist = JSON.parse(localStorage.getItem('blacklist'));
   for (var i = 0; i<blacklist.length; i++){
-    document.getElementById('blacklist')
-    .insertAdjacentHTML('beforeend', '<div id="newChild"></div>')
-    document.getElementById("blacklist").lastElementChild.innerHTML = blacklist[i];
-    document.getElementById("blacklist").lastElementChild.insertAdjacentHTML('beforeend', '<button type="remove">x</button>');
+    var elem = $('<div class="blackListItem">'+ blacklist[i]+'</div>');
+    var button = $('<button type="remove">x</button>');
+    button.on('click', removeItem);
+    elem.append(button);
+
+    $('#blacklist').append(elem);
   }
 
 }
@@ -12,11 +14,15 @@ function init() {
 function saveOptions(e) {
   e.preventDefault();
   var blacklist = JSON.parse(localStorage.getItem('blacklist'));
-  blacklist.push(document.querySelector("#item").value)
+  blacklist.push(document.querySelector("#item").value);
   localStorage.setItem('blacklist', JSON.stringify(blacklist));
-  document.getElementById('blacklist').insertAdjacentHTML('beforeend', '<div class="blackListItem"></div>')
-  document.getElementById("blacklist").lastElementChild.innerHTML = document.querySelector("#item").value;
-  document.getElementById("blacklist").lastElementChild.insertAdjacentHTML('beforeend', '<button type="remove">x</button>');
+
+  var elem = $('<div class="blackListItem">'+$('#item').val()+'</div>');
+  var button = $('<button type="remove">x</button>');
+  button.on('click', removeItem);
+  elem.append(button);
+
+  $('#blacklist').append(elem);
 }
 
 function removeItem(e){
@@ -34,4 +40,3 @@ function restoreOptions() {
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
-document.querySelector("button").addEventListener("click", removeItem);
